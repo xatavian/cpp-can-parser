b9e6fcbc1ed188c7b7ce9a121fe47caf48cdac61
Avi SZYCHTER <avi.szychter@trialog.com>
2020-01-12 13:22:31 +0100

    Improved CLI parsing, now can possibly execute all planned operations

--- utils/can-parse/can-parse.cpp
+++ utils/can-parse/can-parse.cpp
@@ -3,9 +3,9 @@
 #include <iostream>
 #include <cstring>
 #include <algorithm>
-
+#include <set>
 #include "operations.h"
-
+#include <iomanip>
 
 using namespace CppCAN::can_parse;
 
@@ -22,63 +22,91 @@ static std::string CHECKFRAME_ACTION = "checkframe";
 static std::string PRINTFRAME_ACTION = "printframe";
 
 void showUsage(std::ostream& ostrm, char* program_name) {
-  ostrm << "Usage: " << program_name << " [ACTION] <path/to/file>" << std::endl;
+  ostrm << "Usage: " << program_name << " [ACTION [ARGUMENTS]] <path/to/file>" << std::endl;
   ostrm << "When no action is specified, defaults to " << PRINTFRAME_ACTION << std::endl;
   ostrm << "Possible actions: " << std::endl;
-  ostrm << "\t" << PRINTFRAME_ACTION << "\tPrint the content of the CAN database" << std::endl;
-  ostrm << "\t" << CHECKFRAME_ACTION << "\tCheck different properties of the CAN database" << std::endl;
+  ostrm << "\t" << std::left << std::setw(22) << (PRINTFRAME_ACTION + " [CAN ID]") << "Print the content of the CAN database" << std::endl;
+  ostrm << "\t"              << std::setw(22) << ""                                << "if CAN ID is specified, prints the details of the given frame" << std::endl;
+  ostrm << "\t"              << std::setw(22) << (CHECKFRAME_ACTION + " [CAN ID]") << "Check different properties of the CAN database" << std::endl;
+  ostrm << "\t"              << std::setw(22) << ""                                << "if CAN ID is specified, print the check details of the given frame" << std::endl;
   ostrm << "Currently supported formats: DBC" << std::endl;
 }
 
 
-std::tuple<CanParseAction, std::string> extractAction(int argc, char** argv) {
+std::tuple<CanParseAction, std::string, uint32_t> extractAction(int argc, char** argv) {
   std::vector<std::string> args(argv + 1, argv + argc); // +1 so we ignore the executable name
   
   CanParseAction action = None;
   std::string src_file;
+  uint32_t detail_frame = 0;
 
   if (args.size() < 1) {
     throw CppCAN::can_parse::CanParseException("Not enough arguments");
   }
 
-  size_t i = 0;
+  std::set<std::string> options;
+
+  bool check_action = true;
+  bool check_args = false;
   for(const std::string& arg : args) {
     // First argument, we check for potential actions
-    if(i++ == 0) {
+    if(arg.size() > 0 && arg[0] == '-') {
+      options.insert(arg);
+      continue;
+    }
+    else if(check_action) {
       if(arg == CHECKFRAME_ACTION) {
         action = CheckAll;
+        check_args = true;
+        check_action = false;
         continue;
       }
       else if(arg == PRINTFRAME_ACTION) {
         action = PrintAll;
+        check_args = true;
+        check_action = false;
         continue;
+      }
+      else {
+        action = PrintAll;
+        check_action = false;
+      }
+    }
+    else if(check_args) {
+      check_args = false;
+      
+      try {
+        detail_frame = std::stoul(arg);
+        action = static_cast<CanParseAction>(static_cast<int>(action) + 1);
+        continue;
+      } catch(const std::logic_error& e) {
+        // The argument is not related ....
+        // Probably a file name, we stick to PrintAll/CheckAll
       }
     }
 
-    // Check for options
-    if(arg.size() > 0 && arg[0] == '-') {
-      if(arg == "-h" || arg == "--help") {
-        action = Help;
-      }
-    }
-    // This is a file path (for now)
-    else {
-      src_file = arg;
-    }
+    // At this point, nothing has matched. The only valid thing that this could be
+    // is the file name of the database to parse.
+    src_file = arg;
+  }
+
+  if(options.count("-h") > 0 || options.count("--help")) {
+    action = Help;
   }
 
   if(action != Help && src_file.size() == 0)
     throw CppCAN::can_parse::CanParseException("No source file specified");
 
-  return std::make_tuple(action != None ? action : PrintAll, src_file);
+  return std::make_tuple(action != None ? action : PrintAll, src_file, detail_frame);
 }
 
 int main(int argc, char** argv) {
   std::string src_file;
   CanParseAction action;
+  uint32_t detail_frame;
 
   try {
-    std::tie(action, src_file) = extractAction(argc, argv);
+    std::tie(action, src_file, detail_frame) = extractAction(argc, argv);
   }
   catch(const CanParseException& e) {
     std::cerr << "Invalid use of the program: " << e.what() << std::endl;
@@ -102,6 +130,7 @@ int main(int argc, char** argv) {
     return 2;
   }
 
+  std::cout << (detail_frame == 0 ? "Print all" : "Print CAN ID " + std::to_string(detail_frame)) << std::endl; 
   switch(action) {
     case PrintAll:
       print_all_frames(db);
@@ -116,7 +145,7 @@ int main(int argc, char** argv) {
       break;
     
     default:
-      std::cerr << "Acton not implemented yet." << std::endl;
+      std::cerr << "Action not implemented yet." << std::endl;
       return 3;
   }
   return 0;
