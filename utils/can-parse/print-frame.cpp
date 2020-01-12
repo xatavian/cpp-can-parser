661020b50f7cc51acc0234cbc1dcf306558b365f
Avi SZYCHTER <avi.szychter@trialog.com>
2020-01-12 12:33:32 +0100

    PrintAllFrames now prints a table with a summary of the database

--- utils/can-parse/print-frame.cpp
+++ utils/can-parse/print-frame.cpp
@@ -1,6 +1,13 @@
 #include "operations.h"
 #include "CANDatabaseAnalysis.h"
 #include "CANDatabase.h"
+#include <algorithm>
+#include <iomanip>
+
+/*
+ * Internal pretty-printing parameters
+ */
+static const int INT_COL_SIZE = 10;
 
 void print_info_separator(int level = 0) {
   static const char* level_0_separator = "##############";
@@ -21,32 +28,24 @@ void print_info_separator(int level = 0) {
     std::cout << *separator << std::endl;
 }
 
-void print_frame_impl(const CANFrame& frame) {
-  auto choicesStr = [](const std::map<unsigned int, std::string>& choices) {
-    std::string result = "";
-    for(const auto& choice: choices) {
-      result += std::to_string(choice.first) + " -> \"" + choice.second + "\", ";
-    }
-    return result;
-  };
-   
-  std::cout << "FRAME[" << frame.name() << "]:\t"  
+void print_frame_impl(const CANFrame& frame, unsigned name_col_size) {   
+  std::cout << std::left << std::setw(name_col_size) << frame.name() << ":\t"  
             << std::hex << std::showbase << frame.can_id() << "/" 
-            << std::dec << std::noshowbase << frame.dlc() << "/" << std::endl
+            << std::dec << std::noshowbase << frame.dlc() << "/"
             << frame.period() << "s" << std::endl;
 
   if(frame.comment().size() > 0) {
-    std::cout << "FRAME[" << frame.name() << "]:\t \"" << frame.comment() << "\"" << std::endl;
+    std::cout << "COMMENT" << frame.name() << ":\t \"" << frame.comment() << "\"" << std::endl;
   }
 }
 
 void print_signal_impl(const CANSignal& sig) {
   std::cout << "SIGNAL[" << sig.name() << "]: " << std::endl;
   std::cout << "\tstart bit:\t" << sig.start_bit() << std::endl;
-  std::cout << "\tlength:\t" << sig.length() << std::endl;
-  std::cout << "\tendianness:\t" << ((sig.endianness() == CANSignal::BigEndian) 
+  std::cout << "\tlength:\t\t" << sig.length() << std::endl;
+  std::cout << "\tendianness:\t\t" << ((sig.endianness() == CANSignal::BigEndian) 
                                       ? "BigEndian" : "LittleEndian") << std::endl;
-  std::cout << "\tsignedness:\t" << ((sig.signedness() == CANSignal::Signed) 
+  std::cout << "\tsignedness:\t\t" << ((sig.signedness() == CANSignal::Signed) 
                                       ? "Signed" : "Unsigned") << std::endl;
   std::cout << "\tscale:\t" << sig.scale() << std::endl;
   std::cout << "\toffset:\t" << sig.offset() << std::endl;
@@ -64,11 +63,42 @@ void print_signal_impl(const CANSignal& sig) {
   }
 }
 
+void print_frame_line(const CANFrame& src, int name_col_size) {
+  std::cout << std::left
+            << std::setw(name_col_size) << src.name() 
+            << std::setw(INT_COL_SIZE)  << std::hex << std::showbase << src.can_id() 
+            << std::setw(INT_COL_SIZE)  << std::dec << src.dlc()
+            << std::setw(INT_COL_SIZE)  << std::to_string(src.period()) + " s"
+            << std::endl;
+}
 
 void CppCAN::can_parse::print_all_frames(CANDatabase& db) {
   size_t i = 0;
+
+  // First, explore the database to find "pretty-printing" parameters
+  int frame_name_maxsize = 12; // At least a reasonable column size
   for(const auto& frame : db) {
-    print_frame_impl(*frame.second);
+    if(frame.second->name().size() > frame_name_maxsize)
+      frame_name_maxsize = frame.second->name().size() + 1;
+  }
+
+  int full_line_size = frame_name_maxsize + INT_COL_SIZE * 3; 
+  std::cout << std::left;
+  std::cout << std::setw(frame_name_maxsize) << "Frame name"
+            << std::setw(INT_COL_SIZE) << "CAN ID" 
+            << std::setw(INT_COL_SIZE) << "DLC"
+            << std::setw(INT_COL_SIZE) << "Period"
+            << std::endl;
+  std::cout << std::setfill('-') << std::setw(full_line_size) << "" << std::endl;
+  std::cout.fill(' ');
+             
+  for(const auto& frame : db) {
+    print_frame_line(*frame.second, frame_name_maxsize);
+  }
+  
+  /*
+  for(const auto& frame : db) {
+    print_frame_impl(*frame.second, frame_name_maxsize);
     print_info_separator(1);
     
     size_t j = 0;
@@ -82,4 +112,5 @@ void CppCAN::can_parse::print_all_frames(CANDatabase& db) {
     if(i++ < db.size() - 1)
       print_info_separator();
   }
+  */
 }
